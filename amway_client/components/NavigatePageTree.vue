4b87269d8848bcde44835dcde5a0d63599407212
ahmetozyoruk <ahmetozyoruk3@hotmail.com>
2021-12-19 22:03:01 +0300

    initial commit

---
+++ amway_client/components/NavigatePageTree.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex flex-row my-2 mb-5">
+    <v-icon class="mx-2" > {{ icons.mdiHomeOutline }} </v-icon>
+    <v-icon class="mx-2" > {{ icons.mdiChevronRight }} </v-icon>
+    <p class="mx-2 my-auto">Menu</p>
+  </div>
+</template>
+
+<script>
+import { mdiHomeOutline,mdiChevronRight } from '@mdi/js';
+export default {
+  data() {
+    return {
+      icons : {
+        mdiHomeOutline,mdiChevronRight
+      },
+    }
+  },
+}
+</script>
---
+++ amway_client/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app light>
+    <AppBar  />
+    <BottomAppBar />
+    <v-main>
+      <v-container>
+        <Nuxt/>
+      </v-container>
+    </v-main>
+    <Footer />
+    <!-- <v-footer app>
+      <Footer/>
+   </v-footer> -->
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  created() {
+    this.$store.dispatch("initApp");
+  },
+}
+</script>
