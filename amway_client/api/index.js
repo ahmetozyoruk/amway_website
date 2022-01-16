ba2911bea5e85e9bb276469f9b54c26d71326de8
ahmetozyoruk <ahmetozyoruk3@hotmail.com>
2022-01-16 17:33:42 +0300

    express graphql added

--- amway_client/api/index.js
+++ amway_client/api/index.js
@@ -1,13 +1,66 @@
-import axios from 'axios'
+var express = require("express");
+var graphqlHTTP = require("express-graphql");
+var { buildSchema } = require("graphql");
+var { getProducts, addProduct } = require("./data/products");
 
-export const Apisful = axios.create({
-  // We are using apisful.com as a backend for this e-commerce website
-  baseURL: '/api/data/ProductData.json',
-  headers: {
-    // Put your access key here
-    'X-Api-Key': 'IE7Bufk1NgFOBZwc6PxTOIqUB5AFLYf4Ennv4xvO5m4'
+var cors = require("cors");
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Product {
+    id: Int,
+    name: String,
+    GuestPrice: String,
+    OrderingNumber: String,
+    Url: String
   },
-  validateStatus (status) {
-    return status >= 200 && status < 500
+  type Query {
+    hello: String,
+    products: [Product],
+    product(id: Int!): Product,
+  },
+  type Mutation {
+    createProduct(name: String!, description: String!): String,
   }
-})
+`);
+
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world!";
+  },
+  products: () => {
+    return getProducts();
+  },
+  product: ({ id }) => {
+    const products = getProducts();
+    return products.find(p => p.id === id);
+  },
+  createProduct: args => {
+    const { name, description } = args;
+    const newProduct = addProduct(name, description);
+    return `Created: ${newProduct.id} ${newProduct.name} - ${
+      newProduct.description
+    }`;
+  },
+};
+
+var app = express();
+app.use(cors());
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+app.listen(4000);
+console.log("Running a GraphQL API server at localhost:4000/graphql");
+
+// query with list CHECK
+// query with param, CHECK
+// mutator, CREATE, DELETE, UPDATE , CHECK
+
+// correct broken images on NGRX
